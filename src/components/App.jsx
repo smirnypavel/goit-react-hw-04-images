bc34802b0cc231bdb4b6f6fcca3a7ba242568285
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-03-16 11:53:33 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,39 +3,36 @@ import Searchbar from './Searchbar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ImageGallery from './ImageGallery';
-import fetchImages from 'components/FetchData';
+import fetchImages from 'servises/FetchData';
 import Button from 'components/Button';
 import Modal from './Modal';
 import Loader from 'components/Loader/Loader';
 
 export function App() {
-  const [isMounted, setIsMounted] = useState(true);
   const [request, setRequest] = useState('');
   const [response, setResponse] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(false);
   const [totalHits, setTotalHits] = useState(0);
   const [currentImage, setCurrentImage] = useState('');
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    if (isMounted) {
-      setIsMounted(false);
-    } else {
-      setLoading(true);
-      fetchImages(request, currentPage)
-        .then(response => {
-          if (response.hits.length === 0) {
-            throw new Error('No photos found');
-          }
-          setResponse(prevResponse => [...prevResponse, ...response.hits]);
-          setTotalHits(response.totalHits);
-        })
-        .catch(err => setError(err.message))
-        .finally(() => setLoading(false));
+    if (request === '') {
+      return;
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    setLoading(true);
+    fetchImages(request, currentPage)
+      .then(response => {
+        if (response.hits.length === 0) {
+          throw new Error('No photos found');
+        }
+        setResponse(prevResponse => [...prevResponse, ...response.hits]);
+        setTotalHits(response.totalHits);
+      })
+      .catch(err => setError(err.message))
+      .finally(() => setLoading(false), setError(false));
   }, [request, currentPage]);
 
   const handleFormSubmit = query => {
